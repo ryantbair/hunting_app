abc7b059c833f0d4dee897ecc881c2cc320c4a8f
ryantbair <ryanbairess@gmail.com>
2025-09-21 23:26:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask, request, jsonify
 from datetime import date, datetime
 import json
 import os
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for cross-origin requests
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), '.')
 
@@ -66,6 +64,14 @@ def current_seasons_for_wmu(wmu, waterfowl_zone, today=None):
                 results.append(out)
                 break
     return results
+
+@app.after_request
+def add_cors_headers(response):
+    # Allow cross-origin requests from any domain
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
 @app.route('/v1/seasons', methods=['GET'])
 def seasons_lookup():
